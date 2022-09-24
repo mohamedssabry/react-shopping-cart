0230f5ef4bc830f4b6f5e51b6691b15978e267bd
Mohamed Sabry <bola13368@gmail.com>
2022-09-24 23:38:23 +0200

    footer comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,7 +2,11 @@ const gulp = require("gulp");
 const gulpsass = require("gulp-sass");
 const sass = gulpsass(require("sass"));
 
-
-gulp.task("sass", async function () {
-  gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
+gulp.task("watch", async function () {
+  gulp.watch("src/components/**/*.scss", async function () {
+    gulp
+      .src("src/components/**/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("src/css"));
+  });
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="layout">
-      <Header/>
+      <Header />
       <main>content</main>
-      <footer>footer</footer>
+      <Footer />
     </div>
   );
 }
