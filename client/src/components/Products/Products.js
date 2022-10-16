f00874e8524109ac70878332e53b1307d08adbfe
Mohamed <bola13368@gmail.com>
2022-10-17 01:46:45 +0200

    redux product

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../css/Products/Products.css";
 import ProductModal from "./ProductModal";
-import Bounce from "react-reveal/Bounce"
+import Bounce from "react-reveal/Bounce";
+import { connect } from "react-redux";
+import { fetchProducts } from "../../store/actions/product";
 
 function Products(props) {
   const [product, setProduct] = useState("");
+
   const openModal = (product) => {
     setProduct(product);
   };
@@ -12,29 +15,39 @@ function Products(props) {
     setProduct(false);
   };
 
+  useEffect(() => {
+    props.fetchProducts();
+  },[]);
+
   return (
     <Bounce left cascade>
       <div className="products-wrapper">
-        {props.products.map((product) => {
-          return (
-            <div className="product-item" key={product.id}>
-              <a href="#/" onClick={() => openModal(product)}>
-                <img src={product.imgUrl} alt={product.title} />
-              </a>
-              <div className="product-desc">
-                <p>{product.title}</p>
-                <span>${product.price}</span>
-              </div>
-              <button onClick={() => props.addToCart(product)}>
-                Add To Cart
-              </button>
-            </div>
-          );
-        })}
+        {props.products && props.products.length
+          ? props.products.map((product) => {
+              return (
+                <div className="product-item" key={product.id}>
+                  <a href="#/" onClick={() => openModal(product)}>
+                    <img src={product.imgUrl} alt={product.title} />
+                  </a>
+                  <div className="product-desc">
+                    <p>{product.title}</p>
+                    <span>${product.price}</span>
+                  </div>
+                  <button onClick={() => props.addToCart(product)}>
+                    Add To Cart
+                  </button>
+                </div>
+              );
+            })
+          : "loading.."}
         <ProductModal product={product} closeModal={closeModal} />
       </div>
     </Bounce>
   );
 }
 
-export default Products;
+export default connect((state) => {
+  return {
+    products: state.products.products,
+  };
+}, {fetchProducts})(Products);
